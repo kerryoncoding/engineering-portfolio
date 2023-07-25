4b614b04513406d24a21a7bdec80a3cff60708b6
Kerry Guarino <kerry.guarino@gmail.com>
2023-07-24 20:32:43 -0400

    installed react-router-dom, added Route & Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react"
+import {Route, Switch} from "react-router-dom"
 import NavBar from "./components/NavBar"
+import Home from "./components/Home"
+import About from "./components/About"
+import Projects from "./components/Projects"
+
 import './App.css';
 
 function App() {
@@ -7,6 +12,17 @@ function App() {
     <div className="App">
       <h1>Hello World!</h1>
       <NavBar />
+      <Switch>
+        <Route exact path="/about">
+          <Home/>
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/about">
+          <Projects />
+        </Route>
+      </Switch>
     </div>
   );
 }
