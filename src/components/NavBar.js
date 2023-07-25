3d7877089ae793661bf3e933fc43ae70978bf706
Kerry Guarino <kerry.guarino@gmail.com>
2023-07-24 20:57:44 -0400

    added links to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,13 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 
 function NavBar(){
    return (
-         <h2>NavBar</h2>
+      <div>
+         <NavLink exact to="/">HOME</NavLink>
+         <NavLink exact to="/about">ABOUT</NavLink>
+         <NavLink exact to="/projects">PROJECTS</NavLink>
+      </div>
    )
 }
 
