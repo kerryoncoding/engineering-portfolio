bffd17f88d88e80cb371e4341e6a57a4e5dc4d21
Kerry Guarino <kerry.guarino@gmail.com>
2023-07-24 20:26:53 -0400

    placeholder components for Home, About, Projects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from "react"
+import NavBar from "./components/NavBar"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <NavBar />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
 import React from "react"
 
 function NavBar(){
-   <h1>NavBar</h1>
+   return (
+         <h2>NavBar</h2>
+   )
 }
 
 export default NavBar;
