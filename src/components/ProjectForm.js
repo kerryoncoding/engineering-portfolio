dd6d6f9ad1487bba5157928c37bbc963a9749075
Kerry Guarino <kerry.guarino@gmail.com>
2023-07-28 20:11:47 -0400

    detailing form placeholder

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -2,8 +2,27 @@ import React from "react"
 
 
 function ProjectForm(){
+
+
    return (
-      <p>form</p>
+      <div>
+         <h2>Add A Project</h2>
+         <form className="projectForm">
+            <span>
+               <label>Name: </label>
+               <input type="text" id="name" />
+            </span>
+            <span>
+               <label>Image: </label>
+               <input type="text" id="image" />
+            </span>
+            <span>
+               <label>Description: </label>
+               <input type="text" id="description" />
+            </span>
+         </form>
+      </div>
+      
    )  
 }
 
