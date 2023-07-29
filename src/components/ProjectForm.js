0d10164f08ffafc6ccaf368e0b9ec6e6935ae81f
Kerry Guarino <kerry.guarino@gmail.com>
2023-07-29 08:46:54 -0400

    submissions to form will console log

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -1,31 +1,34 @@
 import React, {useState} from "react"
 
 
-
 function ProjectForm(){
    const [name, setName] = useState("")
    const [description, setDescription] = useState("")
    const [image, setImage] = useState("")
 
+   function handleForm(e){
+      e.preventDefault()
+      console.log(name, description, image)
+   }
 
    return (
       <div className="formContainer">
          <h2>Add A Project</h2>
-         <form className="projectForm">
+         <form className="projectForm" onSubmit={handleForm}>
             <div>
                <label>Name: </label>
                <br />
-               <input type="text" id="name" />
+               <input type="text" id="name" onChange={(e)=>setName(e.target.value)}/>
             </div>
             <div>
                <label>Image: </label>
             <br />
-               <input type="text" id="image" />
+               <input type="text" id="image" onChange={(e)=>setImage(e.target.value)} />
             </div>
             <div>
                <label>Description: </label>
             <br />
-               <input type="text" id="description" />
+               <input type="text" id="description" onChange={(e)=>setDescription(e.target.value)} />
             </div>
             <div>
                <button type="submit">Submit</button>
