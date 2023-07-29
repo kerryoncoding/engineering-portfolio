38420ccce85d5abf17146e3c49505a84d3a8b42b
Kerry Guarino <kerry.guarino@gmail.com>
2023-07-29 11:04:44 -0400

    stylized home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ function Home(){
       <div>
          <h2>Hello, I am</h2>
          <h1>Kerry Guarino</h1>
+         <img src="http://www.kerryoncoding.com/images/kerryavatar.png" alt="kerry avitar" className="homeImage" />
          <h3>Engineer. Problem Solver. Lifelong Learner.</h3>
-         <img src="http://www.kerryoncoding.com/images/kerryavatar.png"/>
       </div>
    )
 }
