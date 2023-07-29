094d783b62451d5a67e0b38203ffef7cf6d9d572
Kerry Guarino <kerry.guarino@gmail.com>
2023-07-29 10:58:38 -0400

    added content to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import React from "react"
 function Home(){
    return (
       <div>
-         <h2>Home</h2>
+         <h2>Hello, I am</h2>
+         <h1>Kerry Guarino</h1>
+         <h3>Engineer. Problem Solver. Lifelong Learner.</h3>
+         <img src="http://www.kerryoncoding.com/images/kerryavatar.png"/>
       </div>
    )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom"
 function NavBar(){
    return (
       <nav>
-         <NavLink className="navlink" exact to="/" >HOME</NavLink>
-         <NavLink className="navlink" exact to="/about" >ABOUT</NavLink>
-         <NavLink className="navlink" exact to="/projects" >PROJECTS</NavLink>
+         <NavLink exact to="/" >HOME</NavLink>
+         <NavLink exact to="/about" >ABOUT</NavLink>
+         <NavLink exact to="/projects" >PROJECTS</NavLink>
       </nav>
    )
 }
