9bc2b137ad53a4db75ee2e2c6c0e437e2152f1e2
Kerry Guarino <kerry.guarino@gmail.com>
2023-07-30 15:40:27 -0400

    adjusted text on about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About(){
    return (
       <div className="about-container">
          <h2>ABOUT</h2>
-         <p>I have a strong background in mechanical engineering and have been developing consumer, industrial and medical products for over 15 years.  I have touched on all aspects of product development from initial concept through to manufacturing. It brings me joy to see a design evolve into a product.</p>
+         <p>I have a strong background in mechanical engineering and have been developing consumer, industrial and medical products for over 15 years.  I have touched on all aspects of product development from initial concept through to manufacturing. I love to see a design evolve into a product. A few examples of what I have worked on are shown on the Projects page.</p>
          <div className="skill-list">
             <h3>Skills:</h3>
             <ul>
