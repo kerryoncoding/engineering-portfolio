a131ceba92ba88e26f3cc597614c5eecd71000ec
Kerry Guarino <kerry.guarino@gmail.com>
2023-08-14 13:58:30 -0400

    added Challenge for live code project review

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import React from "react"
+import Challenge from "./Challenge"
 
 function Home(){
+
    return (
       <div>
+         <Challenge />
          <h2>Hello, I am</h2>
          <h1>Kerry Guarino</h1>
          <img src="http://www.kerryoncoding.com/images/kerryavatar.png" alt="kerry avitar" className="homeImage" />
